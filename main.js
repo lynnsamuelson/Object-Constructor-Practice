3d86c7d7950102e0779179cb437ece7ce5371e1c
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-07-20 12:54:53 -0500

    modified index.html and main.js to fit the confines of the assignmnet.

--- main.js
+++ main.js
@@ -7,20 +7,37 @@
 4. for each tree in tree_array, add a block element to the tree-holder div that displays the tree's attributes*/
 
 
-var Tree = function(name, ["leaf shape"], region, ["flower color"]) {
+var Tree = function(name, leafShape, region, flower_color) {
 	this.name = name;
-	this.leaf_shape = ["leaf shape"];
+	this.leafShape = leafShape;
 	this.region = region;
-	this.flowerColor = ["flower color"];
+	this.flowerColor = flower_color;
 }
+// console.log(Tree);
 
 var magnolia = new Tree("Magnolia", "oval", "south-east", "white");
 var apple = new Tree("Apple", "round", "all", "white");
 var cherry = new Tree("Cherry Blossom", "round", "south-east", "pink");
 var evergreen = new Tree("Evergreen", "cylindar", "north", "pine cone");
 
+// console.log(magnolia);
+
 var tree_array = [];
 tree_array.push(magnolia, apple, cherry, evergreen);
 
+console.log([tree_array]);
+
+var outPut = "";
+
+for (var i = 0; i < tree_array.length; i++) {
+	outPut += "<p>" + JSON.stringify(tree_array[i], null, 2) + "</p>";
+}
+
+
 var treeDOM = document.getElementById("tree-holder");
-treeDom.innerHTML = JSON.stringify(treeDOM, null, 2);
+treeDOM.innerHTML = outPut; 
+
+// document.getElementById('magnolia').innerHTML = JSON.stringify("magnolia", null, 2);
+
+// document.getElementById('apple').innerHTML = JSON.stringify("apple", null, 2);
+
